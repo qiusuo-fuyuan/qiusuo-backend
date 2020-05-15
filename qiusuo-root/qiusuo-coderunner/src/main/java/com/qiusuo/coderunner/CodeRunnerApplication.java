55400da7bdd71ad9bd6bdb25fe0e45d79d99911b
Ryan Zheng <nogrethumphrey@gmail.com>
2020-05-15 15:19:45 +0200

    set up the sourdce structure for CodeRunnerService

---
+++ qiusuo-root/qiusuo-coderunner/src/main/java/com/qiusuo/coderunner/CodeRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.qiusuo.coderunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CodeRunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeRunnerApplication.class, args);
+    }
+}
