2d6fd2528fa9c4425a499c9ef09b87d19a803195
Ryan Zheng <nogrethumphrey@gmail.com>
2020-05-21 21:55:08 +0200

    added docker config for elk,mysql

--- qiusuo-root/nacosserver/docker/startup.sh
+++ qiusuo-root/nacosserver/docker/startup.sh
@@ -1,3 +1,4 @@
-git clone --depth 1 https://github.com/nacos-group/nacos-docker.git
+nacos_dir="./nacos-docker"
+[ ! -d "nacos_dir" ] && git clone --depth 1 https://github.com/nacos-group/nacos-docker.git
 cd nacos-docker
 docker-compose -f example/standalone-derby.yaml up
