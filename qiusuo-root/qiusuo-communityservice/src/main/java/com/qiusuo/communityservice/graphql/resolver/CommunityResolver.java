c3d1c4ac5c2b9fc79a4965819f5670ff2ad0a61e
Ryan Zheng <nogrethumphrey@gmail.com>
2020-09-19 20:54:02 +0200

    feature/38-community-api: convert comma separated string into a list

--- qiusuo-root/qiusuo-communityservice/src/main/java/com/qiusuo/communityservice/graphql/resolver/CommunityResolver.java
+++ qiusuo-root/qiusuo-communityservice/src/main/java/com/qiusuo/communityservice/graphql/resolver/CommunityResolver.java
@@ -4,11 +4,15 @@ import com.qiusuo.communityservice.domain.model.Community;
 import graphql.kickstart.tools.GraphQLResolver;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CommunityResolver implements GraphQLResolver<Community> {
     public List<String> tags(Community community) {
-        return null;
+        return Arrays.stream(community.getCommaSeparatedTags()
+                .split(","))
+                .collect(Collectors.toList());
     }
 }
