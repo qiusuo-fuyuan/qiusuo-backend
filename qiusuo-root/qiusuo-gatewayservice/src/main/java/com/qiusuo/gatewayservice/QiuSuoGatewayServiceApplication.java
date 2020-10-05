62e9ec52ff8983dd9a27d961dfda8d0b58877d2b
Ryan Zheng <nogrethumphrey@gmail.com>
2020-10-05 20:37:51 +0200

    feature/16-setup-gateway: (1)added bootstrap.yml (2)added neccessary lib in build.gradle

--- qiusuo-root/qiusuo-gatewayservice/src/main/java/com/qiusuo/gatewayservice/QiuSuoGatewayServiceApplication.java
+++ qiusuo-root/qiusuo-gatewayservice/src/main/java/com/qiusuo/gatewayservice/QiuSuoGatewayServiceApplication.java
@@ -1,5 +1,6 @@
 package com.qiusuo.gatewayservice;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -8,8 +9,16 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class QiuSuoGatewayServiceApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(QiuSuoGatewayServiceApplication.class, args);
+    }
+
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes().build();
     }
+
+
+
+
 }
