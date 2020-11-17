da909bb66404838e6734cf83f103ad9334986675
liyanzhang505 <liyanzhang2018@dingtalk.com>
2020-11-18 00:15:18 +0800

    feature/19-setup-eureka: add discovery configuration of messaging and community service.

--- qiusuo-root/qiusuo-communityservice/src/main/java/com/qiusuo/communityservice/QiuSuoCommunityService.java
+++ qiusuo-root/qiusuo-communityservice/src/main/java/com/qiusuo/communityservice/QiuSuoCommunityService.java
@@ -3,8 +3,9 @@ package com.qiusuo.communityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class QiuSuoCommunityService {
     public static void main(String[] args)  {
--- qiusuo-root/qiusuo-messagingsvr/src/main/java/com/qiusuo/messaging/QiuSuoMessagingServer.java
+++ qiusuo-root/qiusuo-messagingsvr/src/main/java/com/qiusuo/messaging/QiuSuoMessagingServer.java
@@ -2,7 +2,9 @@ package com.qiusuo.messaging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class QiuSuoMessagingServer {
     public static void main(String[] args)  {
