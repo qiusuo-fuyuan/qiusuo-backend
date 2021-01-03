f8a153a4fd1cc9485eefb4849a14ae31baf58257
Ryan Zheng <nogrethumphrey@gmail.com>
2021-01-03 22:33:28 +0100

    feature/111-migrate-gradle-to-kotlin: migrate gradle from groovy to kotlin

---
+++ qiusuo-root/qiusuo-authorizationserver/src/main/java/com/qiusuo/authorizationserver/QiuSuoAuthorizationServer.java
@@ -0,0 +1,11 @@
+package com.qiusuo.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiuSuoAuthorizationServer {
+    public static void main(String[] args) {
+        SpringApplication.run(QiuSuoAuthorizationServer.class, args);
+    }
+}
